2096624be8f1c2032c869e02aa46dcb4f1052c7c
amit-roy1999 <ar0225749@gamil.com>
2020-12-11 21:15:30 +0530

    commet

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles/main.scss";
+import "@fortawesome/fontawesome-free/css/all.css";
+// import "./styles/fontawsome.scss";
+import * as Popper from "@popperjs/core";
+import { createPopper } from "@popperjs/core";
+import bootstrap from "bootstrap";
